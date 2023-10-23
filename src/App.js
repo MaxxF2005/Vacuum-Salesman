0a0b802a94d308834e3c62318f5e02b849ec190e
Maxx Franklin <maxxf2005@gmail.com>
2023-10-23 01:12:38 +0000

    changed title text

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>spooky vacuum</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload, buddy!
         </p>
         <a
           className="App-link"
